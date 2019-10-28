fae40140926a6048f3bb8f5dafe2b89efbafda32
Steven LIAO <stevenliao19900326@gmail.com>
2019-10-28 18:45:52 +1100

    Update category debugging...

--- routes/category.js
+++ routes/category.js
@@ -26,33 +26,33 @@ router.post('/list', async (req, res) => {
 // Create a new category
 router.post('/add', (req, res, next) => {
     Category.find({ name: req.body.name }).then(category => {
-            if(category.length >= 1) {
-                return res.status(409).json({
-                    message: 'This category has already been added!'
-                });
-            } else {
-                    const category = new Category({
-                        parentId: req.body.parentId,
-                        name: req.body.name,
+        if (category.length >= 1) {
+            return res.status(409).json({
+                message: 'This category has already been added!'
+            });
+        } else {
+            const category = new Category({
+                parentId: req.body.parentId,
+                name: req.body.name,
+            });
+
+            category.save()
+                .then(result => {
+                    console.log(result);
+                    res.status(201).json({
+                        message: 'Category added!'
                     });
-                    
-                    category.save()
-                        .then(result => {
-                            console.log(result);
-                            res.status(201).json({
-                                message: 'Category added!'
-                            });
-                        })
-                        .catch(err => {
-                            res.status(500).json({
-                                message: err
-                            });
-                        });
-                }
-            }
-        ).catch(err => {
-            console.log(err);
-        });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        message: err
+                    });
+                });
+        }
+    }
+    ).catch(err => {
+        console.log(err);
+    });
 });
 
 // delete a category
@@ -77,11 +77,12 @@ router.post('/update', (req, res, next) => {
                 { name: req.body.name },
             );
             res.json('Update sccuessful!', updateCategory);
-    
+
         } catch (err) {
             res.json({ message: err });
         }
     });
 });
+
 
 module.exports = router;
